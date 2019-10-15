ff091bc4cbaf6856787b20534766f84e0d1d3f47
omi <freedom2028@gmail.com>
2019-10-15 14:58:09 +0800

    fix($lint): fix vue eslint problem

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   // https://stackoverflow.com/questions/36002226/parsing-error-the-keyword-import-is-reserved-sublimelinter-contrib-eslint
-  parser: "babel-eslint",
   "parserOptions": {
     "parser": "babel-eslint"
   },
-  extends: 'eslint:recommended',
+  "extends": ["plugin:vue/recommended", "plugin:vue/base"],
   rules: {
     'no-console': 'off',
   },
